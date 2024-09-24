bbc1e38746dc60f884243c0884ca61b695eb619a
Binaya <sbinayaraj@gmail.com>
2024-09-24 16:05:20 +0300

    main slider completed

--- src/app/_components/banner/Banner.tsx
+++ src/app/_components/banner/Banner.tsx
@@ -11,74 +11,84 @@ import slider2 from "../../../../public/assets/slider/slider2.jpg"
 import slider3 from "../../../../public/assets/slider/slider3.jpg"
 import Image from "next/image";
 import gsap from 'gsap'
+const slidesData = [
+  {
+    image: slider1,
+    title: "Where Books Come to Life",
+    description: "Create, Publish, and Dive into a World of Free Literature.",
+  },
+  {
+    image: slider2,
+    title: "Your Digital Library",
+    description: "Share, Discover, and Enjoy Thousands of Books from Authors Worldwide.",
+  },
+  {
+    image: slider3,
+    title: "Read Share & Connect",
+    description: "A Community for Book Lovers to Explore and Publish Stories.",
+  },
+];
 const Banner = () => {
+  const findTitleHeight =()=>{
+    const sliderTitle = document.querySelectorAll('.sliderTitle')[0] as HTMLElement;
+    return sliderTitle.offsetHeight;
+  }
+  useEffect(()=>{
+    window.addEventListener('resize',findTitleHeight);
+    return(()=>{
+      window.removeEventListener('resize',findTitleHeight);
+    })
+  },[])
+  const resetTitlePosition = () => {
+    const height = findTitleHeight();
+    gsap.set(".title1", { y: 0 });
+    gsap.set(".title2", { y: `${height}px` });
+    gsap.set('.subTitle',{x:'100%'})
+  };
   const handleSlideChange = () => {
-    // Animate title1 and title2 on slide change
+    const height = findTitleHeight();
     gsap.to(`.title1`, {
-      y: '-3rem',
+      y: `-${height}px`,
       duration: 1,
     });
     gsap.to(`.title2`, {
-      y: '-3rem',
+      y: `-${height}px`,
       duration: 1,
     });
+    gsap.to('.subTitle',{
+      x:0,
+      duration:1,
+    })
+    resetTitlePosition()
   };
   return (
     <Swiper
-    modules={[Navigation, Pagination, Scrollbar, A11y,Autoplay ]} // Load Swiper modules
+    modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]} // Load Swiper modules
     spaceBetween={50}
     slidesPerView={1}
     navigation // Enable navigation for the arrows to work
     pagination={{ clickable: true }}
     scrollbar={{ draggable: true }}
-    onSwiper={(swiper) => console.log(swiper)}
     onSlideChange={handleSlideChange}
     autoplay={{ delay: 3000, disableOnInteraction: false }}
     className="h-auto max-h-[90vh] w-full"
   >
-  <SwiperSlide>
-  <div className="slideImage relative">
-    <Image src={slider1} alt="slider1" className="object-cover" />
-    <div className="slideOverlay absolute top-0 left-0 w-full h-full bg-gradient-to-t from-[rgba(148,163,184,0)] via-[rgba(139,92,246,0.75)] to-[rgba(100,116,139,0.90)]">
-      <div className="absolute top-0 left-0 w-full p-4 text-white">
-        <div className="sliderTitle bg-red-500  w-full h-[3rem] overflow-hidden">
-          <h2 className="text-5xl z-10 uppercase title1">Where Books Come to Life</h2>
-          <h2 className="text-5xl z-10 uppercase title2">Where Books Come to Life</h2>
+    {slidesData.map((slide, index) => (
+      <SwiperSlide key={index}>
+        <div className="slideImage relative">
+          <Image src={slide.image} alt={`slider${index + 1}`} className="object-cover" />
+          <div className="slideOverlay absolute top-0 left-0 w-full h-full bg-gradient-to-t from-[rgba(148,163,184,0)] via-[rgba(139,92,246,0.75)] to-[rgba(100,116,139,0.90)]">
+            <div className="absolute top-0 left-0 w-full p-4 text-white">
+              <div className="sliderTitle w-full  md:h-[3rem] h-[2rem] overflow-hidden">
+                <h2 className="text-2xl sm:text-3xl md:text-5xl z-10 uppercase title1">{slide.title}</h2>
+                <h2 className="text-2xl sm:text-3xl md:text-5xl z-10 uppercase title2">{slide.title}</h2>
+              </div>
+              <p className="text-xl subTitle translate-x-[100%]">{slide.description}</p>
+            </div>
+          </div>
         </div>
-        <p className="text-xl">Create, Publish, and Dive into a World of Free Literature.</p>
-      </div>
-    </div>
-  </div>
-</SwiperSlide>
-<SwiperSlide>
-  <div className="slideImage relative">
-    <Image src={slider2} alt="slider2" className="object-cover" />
-    <div className="slideOverlay absolute top-0 left-0 w-full h-full bg-gradient-to-t from-[rgba(148,163,184,0)] via-[rgba(139,92,246,0.75)] to-[rgba(100,116,139,0.90)]">
-      <div className="absolute top-0 left-0 w-full p-4 text-white">
-        <div className="sliderTitle bg-red-500 overflow-hidden">
-          <h2 className="text-5xl z-10 uppercase w-full h-[4rem] overflow-hidden">Your Digital Library</h2>
-          <h2 className="text-5xl z-10 uppercase">Your Digital Library</h2>
-        </div>
-        <p className="text-xl">Share, Discover, and Enjoy Thousands of Books from Authors Worldwide.</p>
-      </div>
-    </div>
-  </div>
-</SwiperSlide>
-<SwiperSlide>
-  <div className="slideImage relative">
-    <Image src={slider3} alt="slider3" className="object-cover" />
-    <div className="slideOverlay absolute top-0 left-0 w-full h-full bg-gradient-to-t from-[rgba(148,163,184,0)] via-[rgba(139,92,246,0.75)] to-[rgba(100,116,139,0.90)]">
-      <div className="absolute top-0 left-0 w-full p-4 text-white">
-        <div className="sliderTitle bg-red-500 overflow-hidden">
-          <h2 className="text-5xl z-10 uppercase w-full h-[4rem] overflow-hidden">Read, Share, Connect</h2>
-          <h2 className="text-5xl z-10 uppercase">Read, Share, Connect</h2>
-        </div>
-        <p className="text-xl">A Community for Book Lovers to Explore and Publish Stories.</p>
-      </div>
-    </div>
-  </div>
-</SwiperSlide>
-    {/* Add as many slides as needed */}
+      </SwiperSlide>
+    ))}
   </Swiper>
   );
 };
